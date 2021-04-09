1ce40f7b6b63967174aa99499eedb960354a2bd3
bartek9105 <bartek9105@gmail.com>
2021-04-09 13:46:53 +0200

    create full search bar component with overlay

---
+++ src/components/AppSearchFull.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div class="overlay"></div>
+    <div class="search-wrapper">
+      <div class="search">
+        <input type="text" />
+        <button></button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppSearchFull'
+}
+</script>
+
+<style lang="scss" scoped>
+.overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+}
+.search-wrapper {
+  background-color: #fff;
+  width: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 2;
+}
+.search {
+  max-width: 1248px;
+  margin: 0 auto;
+  padding: 5.625rem 0;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,14 @@
 <template>
   <div>
     <app-navbar />
+    <app-search-full v-if="searchFull" />
     <apartments-list :apartments="apartments" />
   </div>
 </template>
 
 <script>
 import AppNavbar from '@/components/AppNavbar'
+import AppSearchFull from '@/components/AppSearchFull'
 import ApartmentsList from '@/components/Apartments/ApartmentsList'
 import stays from '@/_data/stays.json'
 
@@ -14,11 +16,13 @@ export default {
   name: 'Home',
   components: {
     AppNavbar,
+    AppSearchFull,
     ApartmentsList
   },
   data() {
     return {
-      apartments: stays
+      apartments: stays,
+      searchFull: true
     }
   }
 }
